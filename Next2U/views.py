15e02bbd57b5093bc06d8cdc5ae326b5fb3c1a9f
nshukla <nitushukla@yahoo.com>
2015-08-28 22:25:33 -0700

    agencypost

--- Next2U/views.py
+++ Next2U/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+import sqlite3
 
 # Create your views here.
 def index(request):
+    return render(request, 'Next2U/index.html')
+
+def agencypost(request):
+    conn = sqlite3.connect('django.db')
+    c=conn.cursor()
+    c.execute("INSERT INTO Agency VALUES ('Nitu','Shukla','testing','Address1','Address2','city','state','zip','phone','email')")
+    conn.commit()
+    conn.close()
     return render(request, 'Next2U/index.html')
