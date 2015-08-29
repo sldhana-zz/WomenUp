63ece2fd79aaf42e94fbcfb7b081902a3f4b865c
Dhana Shunmugasundram <dhanas@surveymonkey.com>
2015-08-28 22:03:25 -0700

    CSS styes

--- Next2U/views.py
+++ Next2U/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'Next2U/index.html')
+
+def agency_registration(request):
+    return render(request, 'Next2U/agency.html')
