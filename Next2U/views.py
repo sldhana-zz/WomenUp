fbfcc8743ca7c53aa6bb38d595ef457679825f8e
Dhana Shunmugasundram <dhanas@surveymonkey.com>
2015-08-28 22:35:31 -0700

    saving

--- Next2U/views.py
+++ Next2U/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import sqlite3
 
 # Create your views here.
 def index(request):
@@ -9,3 +10,13 @@ def agency_registration(request):
 
 def mentor_registration(request):
     return render(request, 'Next2U/mentor.html')
+
+def agencypost(request):
+    import ipdb
+    ipdb.set_trace()
+    conn = sqlite3.connect('db.sqlite3')
+    c=conn.cursor()
+    c.execute("INSERT INTO Next2U_agency VALUES (1, 'Nitu','Shukla','testing','Address1','Address2','city','state','zip','phone','email')")
+    conn.commit()
+    conn.close()
+    return render(request, 'Next2U/index.html')
