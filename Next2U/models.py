29c43b3a6913a180c3574f3964967bb3167e1493
PriyaB <priya.bonthu@gmail.com>
2015-08-28 19:48:05 -0700

    missed file

--- Next2U/models.py
+++ Next2U/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+# Supporter
+
+class Supporter(models.Model):
+    FirstName = models.CharField(max_length=50)
+    LastName = models.CharField(max_length=50)
+    Address1 = models.CharField(max_length=50)
+    Address2 = models.CharField(max_length=50)
+    City = models.CharField(max_length=50)
+    State = models.CharField(max_length=50)
+    ZIP = models.CharField(max_length=50)
+    Phone = models.CharField(max_length=50)
+    Email = models.CharField(max_length=50)
+
+
+# Agency
+class Agency(models.Model):
+    Name = models.CharField(max_length=50)
+    ContactName = models.CharField(max_length=50)
+    TINNumber = models.CharField(max_length=50)
+    Address1 = models.CharField(max_length=50)
+    Address2 = models.CharField(max_length=50)
+    City = models.CharField(max_length=50)
+    State = models.CharField(max_length=50)
+    ZIP = models.CharField(max_length=50)
+    Phone = models.CharField(max_length=50)
+    Email = models.CharField(max_length=50)
+
+
+
+class Service(models.Model):
+    ServiceName=models.CharField(max_length=50)
+
+
+class SupporterService(models.Model):
+    Supporter= models.ForeignKey(Supporter)
+    Service=models.ForeignKey(Service)
+
+class AgencySupporterApproval(models.Model):
+    Agency=models.ForeignKey(Agency)
+    Supporter=models.ForeignKey(Supporter)
