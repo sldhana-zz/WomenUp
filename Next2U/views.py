2936d90876b4a4ec345867d1e43d1c9c28fde2e4
PriyaB <priya.bonthu@gmail.com>
2015-08-29 01:27:18 -0700

    commiting overwritten changes

--- Next2U/views.py
+++ Next2U/views.py
@@ -23,16 +23,18 @@ def signinDisplay(request):
 
 def agencyRegistrationPost(request):
     agencySaved=Agency(
-    Name = request.POST.get("Name"),
-    ContactName = request.POST.get("ContactName"),
-    TINNumber = request.POST.get("TINNumber"),
-    Address1 = request.POST.get("Address1"),
-    Address2 = request.POST.get("Address2"),
-    City = request.POST.get("City"),
-    State = request.POST.get("State"),
-    ZIP = request.POST.get("ZIP"),
-    Phone = request.POST.get("Phone"),
-    Email = request.POST.get("Email"))
+        Username = request.POST.get("username"),
+        Password = request.POST.get("password"),
+        Name = request.POST.get("Name"),
+        ContactName = request.POST.get("ContactName"),
+        TINNumber = request.POST.get("TINNumber"),
+        Address1 = request.POST.get("Address1"),
+        Address2 = request.POST.get("Address2"),
+        City = request.POST.get("City"),
+        State = request.POST.get("State"),
+        ZIP = request.POST.get("ZIP"),
+        Phone = request.POST.get("Phone"),
+        Email = request.POST.get("Email"))
 
     agencySaved.save()
     return render(request, 'Next2U/agency.html')
@@ -40,23 +42,28 @@ def agencyRegistrationPost(request):
 
 def mentorRegistrationPost(request):
     mentorSaved=Supporter(
-    FirstName = request.POST.get("FirstName"),
-    LastName = request.POST.get("LastName"),
-    Address1 = request.POST.get("Address1"),
-    Address2 = request.POST.get("Address2"),
-    City = request.POST.get("City"),
-    State = request.POST.get("State"),
-    ZIP = request.POST.get("ZIP"),
-    Phone = request.POST.get("Phone"),
-    Email = request.POST.get("Email"))
+        Username = request.POST.get("username"),
+        Password = request.POST.get("password"),
+        FirstName = request.POST.get("FirstName"),
+        LastName = request.POST.get("LastName"),
+        Address1 = request.POST.get("Address1"),
+        Address2 = request.POST.get("Address2"),
+        City = request.POST.get("City"),
+        State = request.POST.get("State"),
+        ZIP = request.POST.get("ZIP"),
+        Phone = request.POST.get("Phone"),
+        Email = request.POST.get("Email"))
 
     mentorSaved.save()
-    return render(request, 'Next2U/mentor.html')
+    return render(request, 'Next2U/mentorSuccess.html')
 
-def agency_search(request):
+def agencysearch(request):
         # get the blog posts that are published
         supporters = Supporter.objects.filter()
         # now return the rendered template
         return render(request, 'Next2U/agencysearch.html')
 
+def loginPost(request):
+    if (request.POST.get("type") == "agency"):
+        agency = Agency.objects.filter()
 
